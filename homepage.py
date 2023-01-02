89bbff80521696a53c6845dd80defe1f6cdb497f
Bonnaire Fils Emmanuel <42214912+Teknikp@users.noreply.github.com>
2023-01-02 09:20:42 -0400

    First step

--- homepage.py
+++ homepage.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-df = st.file_uploader(label='Choose your file')
+# Allow the user to upload a file
+uploaded_file = st.file_uploader("Choose a CSV file:")
 
+# Read the CSV file into a dataframe
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    
+    # Display the dataframe
+    st.dataframe(df)
